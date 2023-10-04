14579da290d41ccdaeb5a9de60b1e667b84b836a
Jorik Cronenberg <jcronenberg@suse.de>
2023-10-04 14:23:28 +0200

    Read wicked xmls as Vector of Interfaces
    A single xml can contain multiple interfaces

--- rust/agama-migrate-wicked/src/reader.rs
+++ rust/agama-migrate-wicked/src/reader.rs
@@ -5,11 +5,10 @@ use std::fs;
 use std::io;
 use std::path::PathBuf;
 
-pub fn read_xml(path: PathBuf) -> Result<Interface, quick_xml::DeError> {
+pub fn read_xml(path: PathBuf) -> Result<Vec<Interface>, quick_xml::DeError> {
     let contents = fs::read_to_string(path).expect("Should have been able to read the file");
     // TODO better error handling when xml parsing failed
-    let interface: Interface = from_str(replace_colons(contents).as_str())?;
-    Ok(interface)
+    from_str(replace_colons(contents).as_str())
 }
 
 fn replace_colons(colon_string: String) -> String {
@@ -22,7 +21,8 @@ fn replace_colons(colon_string: String) -> String {
 
 pub async fn read_dir(path: PathBuf) -> Result<Vec<Interface>, io::Error> {
     let interfaces = fs::read_dir(path)?
-        .map(|res| res.map(|e| read_xml(e.path()).unwrap()))
-        .collect::<Result<Vec<_>, io::Error>>()?;
+        .map(|res| res.map(|e| read_xml(e.path()).unwrap()).unwrap())
+        .flatten()
+        .collect::<Vec<_>>();
     Ok(interfaces)
 }
